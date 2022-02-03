90047cb240e0060daf353ab337b70450b249410a
Dhruva <dhruva3223@gmail.com>
2022-02-03 20:14:33 +0530

    Added Image

--- PomodoroTimer.py
+++ PomodoroTimer.py
@@ -3,4 +3,6 @@ from tkinter import *
 window=Tk()
 window.title("Pomodoro by @dhruva3223")
 window.config(padx=100,pady=50,bg="black")
+title_label=Label(text="Timer",fg="white",font=("arial",50),bg="black")
+title_label.grid(column=1,row=0)
 window.mainloop()
